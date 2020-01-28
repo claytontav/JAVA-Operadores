4823e474c31f7ac2037eb15cf74e524c302b9328
Clayton Tavares <54451974+claytongit@users.noreply.github.com>
2020-01-28 17:21:05 -0300

    Add files via upload

---
+++ Operadores/Operadores.java
@@ -0,0 +1,21 @@
+class Operadores{
+    public static void main(String[] args){
+        int resultado = 1 + 2;
+        System.out.println("Soma: " + resultado);
+
+        resultado = resultado - 1;
+        System.out.println("Subtração: " + resultado);
+
+        resultado = resultado * 2;
+        System.out.println("Multiplicação: " + resultado);
+
+        resultado = resultado / 2;
+        System.out.println("Divisão: " + resultado);
+
+        resultado = resultado + 8;
+        System.out.println("Soma: " + resultado);
+
+        resultado = resultado % 7;
+        System.out.println("Resto da divisão: " + resultado);
+    }
+}
